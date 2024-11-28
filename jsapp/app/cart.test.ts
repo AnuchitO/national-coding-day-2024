81955c379b9e7584fd84ac5eb941d8f52cd5a212
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-28 20:52:13 +0700

    print it all true better, but not yet readable

--- jsapp/app/cart.test.ts
+++ jsapp/app/cart.test.ts
@@ -1,5 +1,5 @@
 import { subtotalPrice } from "./cart";
 
-console.log(subtotalPrice(999.99, 3))
-console.log(subtotalPrice(999.99, 2))
-console.log(subtotalPrice(999.99, 1))
+console.log(subtotalPrice(999.99, 3).toFixed(2) === "2999.97")
+console.log(subtotalPrice(999.99, 2).toFixed(2) === "1999.98")
+console.log(subtotalPrice(999.99, 1).toFixed(2) === "999.99")
