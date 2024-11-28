62c274a3185d91a470bcf68c17ede85fc40079ca
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-28 22:05:24 +0700

    product: add test helper expect and test

--- jsapp/app/product.test.ts
+++ jsapp/app/product.test.ts
@@ -1,1 +1,19 @@
 import { getProductPrice, Product } from "./product";
+
+function expect(actual: any) {
+  return {
+    toEqual: (expected: any) => {
+      if (actual === expected) {
+        console.log(" ✅ PASS")
+      } else {
+        console.log(` ❌ FAIL: want ${expected} but got ${actual}`)
+      }
+    }
+  }
+}
+
+function test(title: string, callback: () => void) {
+  console.group(title);
+  callback();
+  console.groupEnd();
+}
