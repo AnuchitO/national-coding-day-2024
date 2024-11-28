89e79fbe0aac87c3c1d392a3b64e6314c27c696d
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-28 20:57:43 +0700

    I want to call it like expect(..).toEqual(..)

--- jsapp/app/cart.test.ts
+++ jsapp/app/cart.test.ts
@@ -8,6 +8,8 @@ import { subtotalPrice } from "./cart";
 // true, true, true. it works! but could be better for readability
 // let's refactor the console log to print ✅ PASS or ❌ FAIL instead of true or false
 
-console.log(subtotalPrice(999.99, 3).toFixed(2) === "2999.97" ? "✅ PASS" : "❌ FAIL")
-console.log(subtotalPrice(999.99, 2).toFixed(2) === "1999.98" ? "✅ PASS" : "❌ FAIL")
-console.log(subtotalPrice(999.99, 1).toFixed(2) === "999.99" ? "✅ PASS" : "❌ FAIL")
+/* console.log(subtotalPrice(999.99, 3).toFixed(2) === "2999.97" ? "✅ PASS" : "❌ FAIL") */
+/* console.log(subtotalPrice(999.99, 2).toFixed(2) === "1999.98" ? "✅ PASS" : "❌ FAIL") */
+/* console.log(subtotalPrice(999.99, 1).toFixed(2) === "999.99" ? "✅ PASS" : "❌ FAIL") */
+
+// I want to call it like expect(..).toEqual(..)
