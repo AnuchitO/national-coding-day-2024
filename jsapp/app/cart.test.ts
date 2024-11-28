0722d85b566684a8546b005d4af8825a4edea4e1
Nong AnuchitO <anuchito.code@gmail.com>
2024-11-28 20:54:01 +0700

    comment it all out try PASS or FAIL print

--- jsapp/app/cart.test.ts
+++ jsapp/app/cart.test.ts
@@ -1,5 +1,9 @@
 import { subtotalPrice } from "./cart";
 
-console.log(subtotalPrice(999.99, 3).toFixed(2) === "2999.97")
-console.log(subtotalPrice(999.99, 2).toFixed(2) === "1999.98")
-console.log(subtotalPrice(999.99, 1).toFixed(2) === "999.99")
+/* console.log(subtotalPrice(999.99, 3).toFixed(2) === "2999.97") */
+/* console.log(subtotalPrice(999.99, 2).toFixed(2) === "1999.98") */
+/* console.log(subtotalPrice(999.99, 1).toFixed(2) === "999.99") */
+
+
+// true, true, true. it works! but could be better for readability
+// let's refactor the console log to print ✅ PASS or ❌ FAIL instead of true or false
